c6de8831bf480100cfeb68f9f14d9451ca839cc5
Tibo Vanheule <mail@tibovanheule.space>
2025-08-08 10:10:11 +0200

    Update __init__.py

--- custom_components/aws_cost/__init__.py
+++ custom_components/aws_cost/__init__.py
@@ -4,26 +4,32 @@ from __future__ import annotations
 import logging
 
 from .const import DOMAIN
-
+from homeassistant.const import   Platform
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup(hass, config):
-    """Set up the AWS Cost component."""
-    hass.data[DOMAIN] = {}
-    return True
-
+PLATFORMS: list[Platform] = [
+    Platform.SENSOR
+]
 
 async def async_setup_entry(hass, config_entry):
     """Set up AWS Cost from a config entry."""
-
-    await hass.config_entries.async_forward_entry_setups(config_entry, [DOMAIN])
-
-    return True
-
-
-async def async_unload_entry(hass, config_entry):
-    """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
+    
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
 
     return True
+
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: IntegrationBlueprintConfigEntry,
+) -> bool:
+    """Handle removal of an entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+
+async def async_reload_entry(
+    hass: HomeAssistant,
+    entry: IntegrationBlueprintConfigEntry,
+) -> None:
+    """Reload config entry."""
+    await hass.config_entries.async_reload(entry.entry_id)
